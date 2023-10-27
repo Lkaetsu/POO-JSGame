1a4821184f09023523111fd5b228081c7271ea33
Lkaetsu <111883767+Lkaetsu@users.noreply.github.com>
2023-10-27 14:52:15 -0300

    0.1

---
+++ Project/Test.js
@@ -0,0 +1,48 @@
+const canvas = document.getElementById('canvas1');
+const ctx= canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+const playerImage = new Image();
+playerImage.src = 'shadow_dog.png'
+const spriteWidth = 575;
+const spriteHeight = 523;
+let frameX = 0;
+let frameY = 0;
+let gameFrame = 0;
+const staggerFrames = 5;
+const spriteAnimations = [];
+const animationStates = [
+    {
+        name: 'idle',
+        frames: 7,
+    },
+    {
+        name: 'jump',
+        frames: 7,
+    }
+];
+animationStates.forEach((state, index)=>{
+    let frames = {
+        loc: [],
+    }
+    for(let j; j < state.frames; j++){
+        let positionX = j * spriteWidth;
+        let positionY = index * spriteHeight;
+        frames.loc.push({x: positionX, y: positionY});
+    }
+    spriteAnimations[state.name] = frames;
+});
+console.log(spriteAnimations);
+
+function animate(){
+    ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations["idle"].loc.length;
+    frameX = spriteWidth * position;
+    ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth,
+    spriteHeight ,0 ,0 , spriteWidth, spriteHeight);
+    
+    gameFrame++;
+    requestAnimationFrame(animate);
+};
+animate();
