a4014371485fbd58de71f80f918fb09c2d3e001d
Lkaetsu <111883767+Lkaetsu@users.noreply.github.com>
2023-10-27 21:51:14 -0300

    0.2

--- Project/Test.js
+++ Project/Test.js
@@ -1,3 +1,9 @@
+let playerState = 'idle';
+const dropdown = document.getElementById('animations');
+dropdown.addEventListener('change', function(e){
+    playerState = e.target.value;
+})
+
 const canvas = document.getElementById('canvas1');
 const ctx= canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 600;
@@ -7,8 +13,7 @@ const playerImage = new Image();
 playerImage.src = 'shadow_dog.png'
 const spriteWidth = 575;
 const spriteHeight = 523;
-let frameX = 0;
-let frameY = 0;
+
 let gameFrame = 0;
 const staggerFrames = 5;
 const spriteAnimations = [];
@@ -20,6 +25,38 @@ const animationStates = [
     {
         name: 'jump',
         frames: 7,
+    },
+    {
+        name: 'fall',
+        frames: 7,
+    },
+    {
+        name: 'run',
+        frames: 9,
+    },
+    {
+        name: 'dizzy',
+        frames: 11,
+    },
+    {
+        name: 'rest',
+        frames: 5,
+    },
+    {
+        name: 'roll',
+        frames: 7,
+    },
+    {
+        name: 'bite',
+        frames: 7,
+    },
+    {
+        name: 'death',
+        frames: 12,
+    },
+    {
+        name: 'getHit',
+        frames: 4,
     }
 ];
 animationStates.forEach((state, index)=>{
@@ -37,9 +74,10 @@ console.log(spriteAnimations);
 
 function animate(){
     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations["idle"].loc.length;
-    frameX = spriteWidth * position;
-    ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth,
+    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations[playerState].loc.length;
+    let frameX = spriteWidth * position;
+    let frameY = spriteAnimations[playerState].loc[position].y;
+    ctx.drawImage(playerImage, frameX, frameY, spriteWidth,
     spriteHeight ,0 ,0 , spriteWidth, spriteHeight);
     
     gameFrame++;
